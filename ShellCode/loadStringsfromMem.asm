fed777a8b8b1d484f67d2cd189ffa1fb5f5f4b2b
sumxtx <137871537+sumxtx@users.noreply.github.com>
2024-12-14 16:29:49 +0100

    why is segginggit statusgit status?, sadge

--- ShellCode/loadStringsfromMem.asm
+++ ShellCode/loadStringsfromMem.asm
@@ -1,15 +1,19 @@
 section .text
 global _start
+
 _start:
-    jmp one 
+	jmp one 
 
 shellcode:
-    pop rsi
-    mov rax,1
-    mov rdi,1
-    mov rdx,12
-    syscall
+	pop rsi
+	mov rax,1
+	mov rdi,1
+	mov rdx,12
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
 
 one:
-call shellcode
-string_msg: db "Hello world", 0xa
+	call shellcode
+	string_msg: db "Hello world",0xa,0 ;;HelloWorld\n\0
--- ShellCode/loadStringsfromMem_nonull.asm
+++ ShellCode/loadStringsfromMem_nonull.asm
@@ -1,35 +1,35 @@
 section .text
 global _start
 _start:
-    jmp one 
+	jmp one 
 
 shellcode:
 
-    ; Put string string to print from stack into the rsi
-    pop rsi
+	; Put string string to print from stack into the rsi
+	pop rsi
 
-    ; Move Syscall = 1
-    ;mov rax,1
-    xor rax,rax     ; eliminate nulls
-    mov al,1        ;
+	; Move Syscall = 1
+	;mov rax,1
+	xor rax,rax     ; eliminate nulls
+	mov al,0x1        ;
 
-    ; Move fd = stdout = 1
-    ;mov rdi,1
-    xor rdi,rdi     ; eliminate nulls
-    add rdi,1       ;
+	; Move fd = stdout = 1
+	;mov rdi,1
+	xor rdi,rdi     ; eliminate nulls
+	add rdi,0x1       ;
 
-    ; Move length = 12
-    ;mov rdx,12
-    xor rdx, rdx    ; eliminate nulls
-    add rdx,12      ;
-    syscall
+	; Move length = 12
+	;mov rdx,12
+	xor rdx, rdx    ; eliminate nulls
+	add rdx,0xc      ;
+	syscall
 
-    ; exit (avoid segfault)
-    xor rax,rax     ;eliminate nulls
-    mov al,60       ; exit syscall
-    xor rdi,rdi     
-    syscall 
+	; exit (avoid segfault)
+	xor rax,rax     ;eliminate nulls
+	mov al,60       ; exit syscall
+	xor rdi,rdi     
+	syscall 
 
 one:
-    call shellcode
-    string_msg db "Hello world", 0xa
+	call shellcode
+	string_msg: db "Hello world",0xa,0 ;;Hello world\n\0
